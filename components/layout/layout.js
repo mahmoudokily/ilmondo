803453fcd21bf6d3427158be83514564b4e1c9a1
Mahmoud Okily <heshamalamam73@gmail.com>
2022-02-02 01:47:25 +0100

    fix apis

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,34 +1,22 @@
-import styles from './layout.module.css'
-import Link from 'next/link'
-import Head from 'next/head'
-import PublicIcon from '@material-ui/icons/Public';
+import styles from "./layout.module.css";
+import Link from "next/link";
+import Head from "next/head";
+import PublicIcon from "@material-ui/icons/Public";
 
 const Layout = ({ children, title = "Ilmondo" }) => (
-    <div className={styles.container}>
-        <Head>
-            <title>{title} </title>
-        </Head>
-        <header className={styles.header}>
-            <Link href="/">
-                <h1>
-                    il Mondo
-                </h1>
+  <div className={styles.container}>
+    <Head>
+      <title>{title} </title>
+    </Head>
+    <header className={styles.header}>
+      <Link href="/">
+        <h1>il Mondo</h1>
+      </Link>
+    </header>
+    <div className={styles.main}> {children}</div>
 
-            </Link>
-
-
-
-
-
-        </header>
-        <div className={styles.main}>        {children}</div>
-
-        <div className={styles.footer}>
-            copyright : Mahmoud Okily 2021
-        </div>
-
-    </div>
-)
-
+    <div className={styles.footer}>copyright : Mahmoud Okily 2021</div>
+  </div>
+);
 
 export default Layout;
